8377cc3b272cd1117ae35135e6d14fbd10bbe04e
Tom Aucamp <tom@rumbleon.com>
2020-04-29 16:44:57 +0000

    asdf

--- models/core/dim_customers.sql
+++ models/core/dim_customers.sql
@@ -16,23 +16,25 @@ customer_orders as (
       min(order_date) as first_order_date,
       max(order_date) as most_recent_order_date,
       count(order_id) as number_of_orders,
-      sum(order_amount) as customer_lifetime_order_amount
+      sum(order_total) as customer_lifetime_order_amount
       
     from {{ ref('fct_orders') }}
     
+    group by 1
 ),
 
 
 final as (
     select
         customers.customer_id,
-        customers.first_name,
-        customers.last_name,
+        -- customers.first_name,
+        -- customers.last_name,
         customers.customer_name,
         first_order_date,
         most_recent_order_date,
-        number_of_orders
-        customer_lifetime_order_amount
+        coalesce(number_of_orders,0) as number_of_orders,
+        coalesce(customer_lifetime_order_amount,0) as lifetime_order_amount
+        
     from customers
     left join customer_orders using (customer_id)
 )
